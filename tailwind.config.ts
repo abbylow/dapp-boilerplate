ebedc2e7e533b7ca3f5e4fdbe79c452c463bf2c1
Abby L <yu950713@gmail.com>
2024-07-19 10:57:25 +0800

    style: choose inter as font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "tailwindcss"
 
+import { fontFamily } from "tailwindcss/defaultTheme"
+
 const config = {
   darkMode: ["class"],
   content: [
@@ -7,7 +9,7 @@ const config = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+  ],
   prefix: "",
   theme: {
     container: {
@@ -71,6 +73,9 @@ const config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
       },
     },
   },
