600a93e0c0abcda1ee995dd010af344ac0a1ee8b
Abby L <yu950713@gmail.com>
2024-07-19 11:37:50 +0800

    config: add env vars and next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push("pino-pretty");
+    return config;
+  },
+  // TODO: change ipfs gateway to env var
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'indigo-capable-clownfish-921.mypinata.cloud',
+        port: '',
+        pathname: '/ipfs/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
